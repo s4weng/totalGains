c022745499d383e81b1d938f5915545458497b22
Steve <steve_weng@hotmail.ca>
2014-10-29 23:50:11 -0400

    implemented basic ability to enter and store lift numbers (works across tabs)

--- www/js/app.js
+++ www/js/app.js
@@ -1,11 +1,23 @@
-// Ionic Starter App
+var WLPApp = angular.module('WLP', ['ionic']);
 
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+WLPApp.controller('WLPCtrl', function($scope) {
 
-.run(function($ionicPlatform) {
+  $scope.lifts = [
+  {name: 'Snatch'},
+  {name: 'Clean and Jerk'},
+  {name: 'Squat'},
+  {name: 'Pull'}
+  ];
+
+  //$scope.lifts = {};
+  
+  $scope.update = function(){
+  alert($scope.wow);
+  };
+
+});
+
+WLPApp.run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
